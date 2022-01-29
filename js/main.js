5c0251f361b5d72e7411fe1f9591bec2faabe0ec
lse1656 <lse1656@naver.com>
2022-01-29 18:32:25 +0900

    💄 Finish style all-product layout

---
+++ js/main.js
@@ -0,0 +1,8 @@
+// const header = document.querySelector('.header');
+// const lnb = document.querySelectorAll('.lnb');
+
+// lnb.forEach((element) => {
+//   element.addEventListener('mouseover', () => {
+//     header.style.height = '320px';
+//   });
+// });
